273f4156c833b2f21d417cdcce8db2d03bde5749
rouamansour <rouamansour.rm@gmail.com>
2022-07-29 10:59:30 +0100

    update app.js

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter } from "react-router-dom";
+import { MainLayout } from "./layouts";
+import { themeL, themeD } from "./themes";
+import { mainNavigation, mainRoutes } from "./data";
+import { RoutesWithLayout } from "./components";
 
-function App() {
+const App = () => {
+  const [darkMode, setDarkMode] = React.useState(() => {
+    const dark = localStorage.getItem("dark");
+    if (dark) {
+      return JSON.parse(dark);
+    } else {
+      return false;
+    }
+  });
+
+  const darkModeToggle = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem("dark", !darkMode);
+  };
+
+  const themeSwitchCofig = {
+    state: darkMode,
+    handler: darkModeToggle,
+  };
+
+  const appliedTheme = createMuiTheme(darkMode ? themeD : themeL);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={appliedTheme}>
+        <RoutesWithLayout
+          layout={MainLayout}
+          routes={mainRoutes}
+          LayoutProps={{
+            navigationData: mainNavigation,
+            themeConfig: themeSwitchCofig,
+          }}
+        />
+      </ThemeProvider>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
